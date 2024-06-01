53cf401415d82abe24573f0399d1468b210c402c
ByteWiizard <ByteWiizard@gmail.com>
2024-06-01 16:21:08 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
-  build: {
-    rollupOptions: {
-      external: ["@emailjs/browser"], // Add your external packages here
-    },
-  },
 })
